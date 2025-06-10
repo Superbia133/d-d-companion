1f180d54a541bf46894c28447d53e0dd4eb5e577
Superbia133 <egriffeth@byupathway.edu>
2025-06-09 17:20:51 -0700

    fix

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const npcRoute = require("./routes/npcRoute");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public"))); // serve /public folder
-app.use("/npc", npcRoute); // mount the /npc route
+app.use(express.static(path.join(__dirname, "public"))); // Serve /public folder
+
+// Mount /npc route
+app.use("/npc", npcRoute);
+
+// Home route
+app.get("/", (req, res) => {
+  res.render("home"); // Will render views/home.ejs
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
